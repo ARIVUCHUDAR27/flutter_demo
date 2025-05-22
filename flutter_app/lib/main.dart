7b66e870b18b5a257591ce1650f311ff5f726127
ARIVUCHUDAR27 <ARIV22002.AD@rmkec.ac.in>
2025-05-22 20:17:29 +0530

    updates

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/login.dart';
 import 'package:flutter_app/pages/onboard.dart';
-import 'package:flutter_app/pages/signup.dart';
 
 void main() {
   runApp(const MyApp());
